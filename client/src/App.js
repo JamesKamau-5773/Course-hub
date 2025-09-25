6cd08abfe555ba15480aa3fedec1d53a5181cdba
Rroon3y <rooneyalex2016@gmail.com>
2025-09-25 15:27:05 +0300

    add

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/NavBar';
+import Courses from './Components/pages/Courses';
+import Students from './Components/pages/Students';
+import Enrollments from './Components/pages/Enrollments';
+import Login from './Components/pages/Login';
+import Signup from './Components/pages/Signup';
+import Dashboard from './Components/pages/Dashboard';
+import "./index.css"
+
