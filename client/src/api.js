3b785b45154df00fd8b81b8832e0d82429997ffc
James Kamau <jamesnk5773@gmail.com>
2025-10-06 17:34:45 +0300

    Update API URL to connect frontend to deployed backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://course-hub-1.onrender.com/api';
+const API_URL = 'https://course-hub-1.onrender.com/api';
 
 const getToken = () => localStorage.getItem('token');
 
